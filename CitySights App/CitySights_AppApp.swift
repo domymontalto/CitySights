0640c2ccd8569bd3f4a6e079bddbe04d0c3da738
Domenico Montalto <montaltodomenico92@gmail.com>
2024-06-20 18:10:30 -0600

    Initial Commit

---
+++ CitySights App/CitySights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySights_AppApp.swift
+//  CitySights App
+//
+//  Created by Domenico Montalto on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CitySights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
